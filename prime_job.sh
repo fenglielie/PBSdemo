2d88d44cfd06004597d36527fd41546cd7530a06
fenglielie <fenglielie@qq.com>
2024-06-04 23:08:02 +0800

    simplify

--- prime_job.sh
+++ prime_job.sh
@@ -4,7 +4,7 @@
 #PBS -N prime_job
 
 # output
-#PBS -o out.$PBS_JOBID
+#PBS -o out_$PBS_JOBID
 #PBS -j oe
 
 # resource
@@ -21,10 +21,6 @@
 #PBS -V
 
 
-# ------------------------------------------------------------------------------ #
-
-# source some scripts
-
 # information
 echo ------------------------------------------------------
 echo [PBS] job id: $PBS_JOBID
@@ -33,12 +29,6 @@ echo [PBS] executing queue: $PBS_QUEUE
 echo [PBS] working directory: $PBS_O_WORKDIR/PBS_$PBS_JOBID
 echo [PBS] home directory: $PBS_O_HOME
 echo ------------------------------------------------------
-echo [PBS] node file: $PBS_NODEFILE
-cat $PBS_NODEFILE
-echo ------------------------------------------------------
-
-
-# ------------------------------------------------------------------------------ #
 
 # directory
 cd $PBS_O_WORKDIR
@@ -46,10 +36,7 @@ mkdir PBS_$PBS_JOBID
 cd PBS_$PBS_JOBID
 
 date
-echo "running..."
 
 $PBS_O_WORKDIR/prime
 
 date
-
-# ------------------------------------------------------------------------------ #
